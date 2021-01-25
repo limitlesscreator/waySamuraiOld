3356ae2aedc22603525046ea9938dd176c6e427e
Vladislav Voloshenko <0427vlad0427@mail.ru>
2021-01-25 23:34:39 +0300

    stage 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
-import {Technologies} from "./Technologies";
-import {Footer} from "./Footer";
+import {Header} from "./components/Header";
 
 
 const App = () => {
     return (
-        <div>
+        <div className='app-wraper'>
             <Header/>
-            <Technologies/>
-            <Footer/>
+            <nav className='nav'>
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://www.53construction.com/wp-content/uploads/2018/11/footer-bg.jpg" alt=""/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );

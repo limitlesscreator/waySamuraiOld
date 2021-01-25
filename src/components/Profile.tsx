54722bf6c978dd09c937da40120119637173eaf0
Vladislav Voloshenko <0427vlad0427@mail.ru>
2021-01-25 23:51:06 +0300

    stage 12

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://www.freepnglogos.com/uploads/logo-website-png/logo-website-website-logo-png-transparent-background-background-15.png"
+                alt=""/>
+        </header>
+    )
+}
